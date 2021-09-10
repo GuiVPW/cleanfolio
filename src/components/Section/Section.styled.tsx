61c9e8f57c17c800c30892fcf51c75cc962abbd3
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-10 03:44:12 -0300

    feat: add styled section and section title components

---
+++ src/components/Section/Section.styled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  margin-top: 5em;
+
+  @media only screen and (max-width: 600px) {
+    margin-top: 4em;
+  }
+`
+
+export const SectionTitle = styled.h2`
+  text-align: center;
+  margin-bottom: 1em;
+  text-transform: uppercase;
+`
