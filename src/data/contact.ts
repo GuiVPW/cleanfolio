916a63d7261b51753da04f4d4614da7ca9b20e1a
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 20:57:16 -0300

    feat: add email info

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,5 +2,5 @@ import { ContactModel } from '../models'
 
 // Email is optional - if left empty Contact section won't show up
 export const contactData: ContactModel = {
-  email: 'johnsmith@mail.com',
+  email: 'guivpw68@gmail.com',
 }
