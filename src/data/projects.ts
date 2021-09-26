f3a5c7a0ebe7e5770bfc0c846ecf0101220fd4c9
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 20:57:43 -0300

    feat: add projects info

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,29 +2,4 @@ import { ProjectModel } from '../models'
 
 // Projects can be added and removed
 // if there are no projects, Projects section won't show up
-export const projectsData: ProjectModel[] = [
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-]
+export const projectsData: ProjectModel[] = []
