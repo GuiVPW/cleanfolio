093038f13eedda494c9acf45a582a39e694f8e01
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 20:57:23 -0300

    feat: add github page header

--- src/data/header.ts
+++ src/data/header.ts
@@ -2,6 +2,6 @@ import { HeaderModel } from '../models'
 
 // All the properties are optional - can be left empty or deleted
 export const headerData: HeaderModel = {
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://guivpw.github.io',
   title: 'TS.',
 }
