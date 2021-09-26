9445a3b41068dad1305ea91698d5d7782c137484
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 20:57:08 -0300

    feat: add about information

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,13 +2,13 @@ import { AboutModel } from '../models'
 
 // All the properties are optional - can be left empty or deleted
 export const aboutData: AboutModel = {
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Guilherme Vieira de Paiva',
+  role: 'Full-stack Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am a huge lover of physics, astronomy & astrophysics. I enjoy creating out of the box projects and huge ideas in simple codes.',
+  // resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/guilhermevp/https://linkedin.com',
+    github: 'https://github.com/GuiVPW',
   },
 }
